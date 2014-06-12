54d4a4a5dbf02abf8b505511e69d6b562aeaa169
Brent Shaffer <bshafs@gmail.com>
2014-06-12 13:43:11 -0600

    fixes #354 - bundles PDO SQL with the library

--- src/OAuth2/Storage/Pdo.php
+++ src/OAuth2/Storage/Pdo.php
@@ -4,6 +4,7 @@ namespace OAuth2\Storage;
 
 use OAuth2\OpenID\Storage\UserClaimsInterface;
 use OAuth2\OpenID\Storage\AuthorizationCodeInterface as OpenIDAuthorizationCodeInterface;
+
 /**
  * Simple PDO storage for all storage types
  *
@@ -13,6 +14,7 @@ use OAuth2\OpenID\Storage\AuthorizationCodeInterface as OpenIDAuthorizationCodeI
  *
  * NOTE: Passwords are stored in plaintext, which is never
  * a good idea.  Be sure to override this for your application
+ *
  *
  * @author Brent Shaffer <bshafs at gmail dot com>
  */
@@ -441,5 +443,110 @@ class Pdo implements AuthorizationCodeInterface,
         }
 
         return 'RS256';
+    }
+
+    /**
+     * DDL to create MySQL 'oauth' database and tables for PDO storage
+     *
+     * @see https://github.com/dsquier/oauth2-server-php-mysql
+     */
+    public function getBackupSql($dbName = 'oauth2_server_php')
+    {
+        // Drop any old backups and create a new backup of tables to be created
+        $sql = "
+        DROP DATABASE IF EXISTS {$dbName}_backup;
+        CREATE DATABASE {$dbName}_backup;
+        USE {$dbName}_backup;
+
+        CREATE TABLE {$this->config['client_table']} AS SELECT * FROM {$dbName}.{$this->config['client_table']};
+        CREATE TABLE {$this->config['access_token_table']} AS SELECT * FROM {$dbName}.{$this->config['access_token_table']};
+        CREATE TABLE {$this->config['code_table']} AS SELECT * FROM {$dbName}.{$this->config['code_table']};
+        CREATE TABLE {$this->config['refresh_token_table']} AS SELECT * FROM {$dbName}.{$this->config['refresh_token_table']};
+        CREATE TABLE {$this->config['user_table']} AS SELECT * FROM {$dbName}.{$this->config['user_table']};
+        CREATE TABLE {$this->config['scope_table']} AS SELECT * FROM {$dbName}.{$this->config['scope_table']};
+        CREATE TABLE {$this->config['public_key_table']} AS SELECT * FROM {$dbName}.{$this->config['public_key_table']};
+        ";
+
+        return $sql;
+    }
+
+    /**
+     * DDL to create MySQL 'oauth' database and tables for PDO storage
+     *
+     * @see https://github.com/dsquier/oauth2-server-php-mysql
+     */
+    public function getBuildSql($dbName = 'oauth2_server_php')
+    {
+        $sql = "
+        CREATE TABLE {$this->config['client_table']} (
+          client_id             VARCHAR(80)   NOT NULL,
+          client_secret         VARCHAR(80)   NOT NULL,
+          redirect_uri          VARCHAR(2000),
+          grant_types           VARCHAR(80),
+          scope                 VARCHAR(4000),
+          user_id               VARCHAR(80),
+          PRIMARY KEY (client_id)
+        );
+
+        CREATE TABLE {$this->config['access_token_table']} (
+          access_token         VARCHAR(40)    NOT NULL,
+          client_id            VARCHAR(80)    NOT NULL,
+          user_id              VARCHAR(80),
+          expires              TIMESTAMP      NOT NULL,
+          scope                VARCHAR(4000),
+          PRIMARY KEY (access_token)
+        );
+
+        CREATE TABLE {$this->config['code_table']} (
+          authorization_code  VARCHAR(40)    NOT NULL,
+          client_id           VARCHAR(80)    NOT NULL,
+          user_id             VARCHAR(80),
+          redirect_uri        VARCHAR(2000),
+          expires             TIMESTAMP      NOT NULL,
+          scope               VARCHAR(4000),
+          id_token            VARCHAR(1000),
+          PRIMARY KEY (authorization_code)
+        );
+
+        CREATE TABLE {$this->config['refresh_token_table']} (
+          refresh_token       VARCHAR(40)    NOT NULL,
+          client_id           VARCHAR(80)    NOT NULL,
+          user_id             VARCHAR(80),
+          expires             TIMESTAMP      NOT NULL,
+          scope               VARCHAR(4000),
+          PRIMARY KEY (refresh_token)
+        );
+
+        CREATE TABLE {$this->config['user_table']} (
+          username            VARCHAR(80),
+          password            VARCHAR(80),
+          first_name          VARCHAR(80),
+          last_name           VARCHAR(80),
+          email               VARCHAR(80),
+          email_verified      BOOLEAN,
+          scope               VARCHAR(4000)
+        );
+
+        CREATE TABLE {$this->config['scope_table']} (
+          scope               VARCHAR(80)  NOT NULL,
+          is_default          BOOLEAN,
+          PRIMARY KEY (scope)
+        );
+
+        CREATE TABLE {$this->config['jwt_table']} (
+          client_id           VARCHAR(80)   NOT NULL,
+          subject             VARCHAR(80),
+          public_key          VARCHAR(2000) NOT NULL
+        );
+
+        CREATE TABLE {$this->config['public_key_table']} (
+          client_id            VARCHAR(80),
+          public_key           VARCHAR(2000),
+          private_key          VARCHAR(2000),
+          encryption_algorithm VARCHAR(100) DEFAULT 'RS256'
+        )
+";
+
+        return $sql;
     }
 }
